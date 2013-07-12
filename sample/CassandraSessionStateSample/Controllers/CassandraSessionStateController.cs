4cc7252e61421102f31637ac29b0210b8d0053c6
Steve Bering <stevebering@gmail.com>
2013-07-12 09:05:31 -0700

    Adding sample project

---
+++ sample/CassandraSessionStateSample/Controllers/CassandraSessionStateController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+
+namespace CassandraSessionStateSample.Controllers
+{
+    public class CassandraSessionStateController : Controller
+    {
+        private const string SessionStateKey = "__key";
+
+        public ActionResult Index() {
+
+            ViewBag.Value = Session[SessionStateKey];
+            return View();
+        }
+
+        public ActionResult SetValue(string value) {
+
+            Session[SessionStateKey] = value;
+            return RedirectToAction("Index");
+        }
+    }
+}
