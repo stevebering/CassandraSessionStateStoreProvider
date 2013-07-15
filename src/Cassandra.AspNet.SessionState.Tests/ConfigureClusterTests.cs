a8a20d0efcc62e19dcd05e5e54aa593f5cb5d1df
Steve Bering <sbering@meracord.com>
2013-07-15 10:36:47 -0700

    Adding tests to verify behavior of Cassandra drivers

---
+++ src/Cassandra.AspNet.SessionState.Tests/ConfigureClusterTests.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Web.SessionState;
+using Cassandra.Data.Linq;
+using NUnit.Framework;
+
+namespace Cassandra.AspNet.SessionState.Tests
+{
+    [TestFixture]
+    public class ConfigureClusterTests
+    {
+        private const string KeySpace = "SessionState_Test";
+
+        private static Cluster CreateCluster() {
+            var builder = Cluster.Builder()
+                .AddContactPoint("127.0.0.1")
+                .WithQueryTimeout(60 * 1000)
+                .WithAsyncCallTimeout(360 * 1000)
+                .WithCompression(CompressionType.Snappy)
+                .WithoutRowSetBuffering();
+
+            return builder.Build();
+        }
+
+        private static Session CreateSession(Cluster cluster) {
+            var session = cluster.Connect();
+
+            session.CreateKeyspaceIfNotExists(KeySpace);
+            session.ChangeKeyspace(KeySpace);
+
+            return session;
+        }
+
+        [Test]
+        public void CanCreateCluster() {
+            var cluster = Cluster.Builder()
+                .AddContactPoint("127.0.0.1")
+                .Build();
+
+            Assert.That(cluster, Is.Not.Null);
+            Assert.That(cluster.Configuration.ClientOptions.WithoutRowSetBuffering, Is.True);
+            Assert.That(cluster.Configuration.ProtocolOptions.Compression, Is.EqualTo(CompressionType.Snappy));
+        }
+
+        [Test]
+        public void CanCreateKeyspace() {
+            var cluster = CreateCluster();
+            var session = cluster.Connect();
+
+            session.CreateKeyspaceIfNotExists(KeySpace);
+            session.ChangeKeyspace(KeySpace);
+
+            Assert.That(session, Is.Not.Null);
+            Assert.That(session.Keyspace, Is.EqualTo(KeySpace));
+        }
+
+        [Test]
+        public void CanCreateTables() {
+            var cluster = CreateCluster();
+            var session = CreateSession(cluster);
+
+            var context = new SessionStateContext(session);
+            context.SaveChanges(SaveChangesMode.Batch);
+
+            Assert.That(context, Is.Not.Null);
+            Assert.That(context.HasTable<UserSession>(), Is.True);
+
+            // clean up
+            DropTables(context, session);
+        }
+
+        private static void DropTables(SessionStateContext context, Session session) {
+            if (context.HasTable<UserSession>()) {
+                var table = context.GetTable<UserSession>();
+                var tableName = table.GetTableName();
+
+                session.Execute(string.Format("DROP TABLE {0};", tableName));
+            }
+        }
+
+        [Test]
+        public void CanInsertUserSession() {
+            var cluster = CreateCluster();
+            var session = CreateSession(cluster);
+
+            // remove table at the start
+            session.Execute("DROP TABLE sessions;");
+
+            var context = new SessionStateContext(session);
+            var table = context.GetTable<UserSession>();
+
+            var countBefore = table.Count().Execute();
+
+            var batch = session.CreateBatch();
+
+            var userSession = new UserSession(Guid.NewGuid().ToString(), "/") {
+                Expires = DateTime.UtcNow.AddMinutes(20),
+                Flags = (int)SessionStateActions.InitializeItem,
+                SessionItems = new object().GetHashCode().ToString(),
+                Locked = false
+            };
+
+            batch.Append(table.Insert(userSession));
+            batch.Execute();
+
+            var countAfter = table.Count().Execute();
+            Assert.That(countAfter, Is.GreaterThan(countBefore));
+        }
+
+        [Test]
+        public void CanInsertLargeNumberOfUserSession() {
+            var cluster = CreateCluster();
+            var session = CreateSession(cluster);
+
+            // remove table at the start
+            session.Execute("DROP TABLE sessions;");
+
+            var context = new SessionStateContext(session);
+
+            var table = context.GetTable<UserSession>();
+
+            var countBefore = table.Count().Execute();
+
+            const int count = 10000;
+            for (int i = 0; i < count; i++) {
+                var userSession = new UserSession(Guid.NewGuid().ToString(), "/") {
+                    Expires = DateTime.UtcNow.AddMinutes(20),
+                    Flags = (int)SessionStateActions.InitializeItem,
+                    SessionItems = new object().GetHashCode().ToString(),
+                    Locked = false
+                };
+
+                table.AddNew(userSession, EntityTrackingMode.DetachAfterSave);
+            }
+
+            context.SaveChanges(SaveChangesMode.Batch);
+
+            var countAfter = table.Count().Execute();
+            Assert.That(countAfter, Is.GreaterThan(countBefore));
+            Assert.That(countAfter, Is.EqualTo(count));
+        }
+
+        [Test]
+        public void MassiveAsyncTest() {
+            var cluster = CreateCluster();
+            var session = CreateSession(cluster);
+
+            var context = new SessionStateContext(session);
+            // remove table at the start
+            DropTables(context, session);
+
+            var table = context.GetTable<UserSession>();
+            table.CreateIfNotExists();
+
+            const int totalRows = 100000;
+            bool[] responses = new bool[totalRows];
+
+            var thread = new Thread(() => {
+                for (int i = 0; i < totalRows; i++) {
+                    int tmpi = i;
+                    Console.Write("+");
+                    var batch = session.CreateBatch();
+                    var userSession = new UserSession(Guid.NewGuid().ToString(), "/") {
+                        Expires = DateTime.UtcNow.AddMinutes(20),
+                        Flags = (int)SessionStateActions.InitializeItem,
+                        SessionItems = new object().GetHashCode().ToString(),
+                        Locked = false
+                    };
+                    batch.Append(table.Insert(userSession));
+                    batch.BeginExecute((_) => {
+                        responses[tmpi] = true;
+                        Thread.MemoryBarrier();
+                    }, null);
+                }
+            });
+
+            thread.Start();
+
+            HashSet<int> done = new HashSet<int>();
+            while (done.Count < totalRows) {
+                for (int i = 0; i < totalRows; i++) {
+                    Thread.MemoryBarrier();
+                    if (!done.Contains(i) && responses[i]) {
+                        done.Add(i);
+                        Console.Write("-");
+                    }
+                }
+            }
+
+            thread.Join();
+
+            Console.WriteLine();
+            Console.WriteLine("Inserted... now we are checking the count");
+
+            var tableName = table.GetTableName();
+            using (var ret = session.Execute(string.Format(@"SELECT * from {0} LIMIT {1};", tableName, totalRows + 100), ConsistencyLevel.Quorum)) {
+                Assert.That(ret.GetRows().Count(), Is.EqualTo(totalRows));
+            }
+        }
+
+        [Test]
+        public void CanInsertAndQueryForSingleItem() {
+            var cluster = CreateCluster();
+            var session = CreateSession(cluster);
+
+            // remove table at the start
+            session.Execute("DROP TABLE sessions;");
+
+            var context = new SessionStateContext(session);
+
+            var table = context.GetTable<UserSession>();
+
+            var uid = Guid.NewGuid().ToString();
+            const string appName = "/";
+            object lockId = 123;
+
+            var userSession = new UserSession(uid, appName) {
+                Expires = DateTime.UtcNow.AddMinutes(20),
+                Flags = (int)SessionStateActions.InitializeItem,
+                SessionItems = new object().GetHashCode().ToString(),
+                Locked = true,
+                LockId = (int)lockId,
+                LockDate = DateTime.UtcNow,
+            };
+
+            table.AddNew(userSession, EntityTrackingMode.KeepAttachedAfterSave);
+
+            context.SaveChanges(SaveChangesMode.OneByOne);
+
+            var locatedSession = table.FirstOrDefault(x => x.SessionId == uid &&
+                                                           x.ApplicationName == appName &&
+                                                           x.LockId == (int)lockId).Execute();
+
+            Assert.That(locatedSession, Is.Not.Null);
+        }
+
+        [Test]
+        public void CanUpdateSingleItem() {
+            var cluster = CreateCluster();
+            var session = CreateSession(cluster);
+
+            // remove table at the start
+            session.Execute("DROP TABLE sessions;");
+
+            var context = new SessionStateContext(session);
+
+            var table = context.GetTable<UserSession>();
+
+            var uid = Guid.NewGuid().ToString();
+            const string appName = "/";
+            object lockId = 123;
+
+            var userSession = new UserSession(uid, appName) {
+                Expires = DateTime.UtcNow.AddMinutes(20),
+                Flags = (int)SessionStateActions.InitializeItem,
+                SessionItems = new object().GetHashCode().ToString(),
+                Locked = true,
+                LockId = (int)lockId,
+                LockDate = DateTime.UtcNow,
+            };
+
+            table.AddNew(userSession, EntityTrackingMode.DetachAfterSave);
+            context.SaveChanges(SaveChangesMode.OneByOne);
+
+            var t2 = context.GetTable<UserSession>();
+            var sessionToUpdate = t2.FirstOrDefault(x => x.SessionId == uid &&
+                                                           x.ApplicationName == appName &&
+                                                           x.LockId == (int)lockId).Execute();
+            t2.Attach(sessionToUpdate);
+
+            sessionToUpdate.LockId += 1;
+            context.SaveChanges(SaveChangesMode.OneByOne);
+
+            // load again and verify the value has changed
+            var t3 = context.GetTable<UserSession>();
+            var loadedSession = t3.FirstOrDefault(x => x.SessionId == uid && x.ApplicationName == appName).Execute();
+
+            Assert.That(loadedSession, Is.Not.Null);
+            Assert.That(loadedSession.LockId, Is.EqualTo(124));
+        }
+    }
+}
---
+++ src/Cassandra.AspNet.SessionState.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Cassandra.AspNet.SessionState.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cassandra.AspNet.SessionState.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d000bf9a-0f2e-4cda-ace4-b9b17bc91dda")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
